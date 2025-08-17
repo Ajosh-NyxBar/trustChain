bf30f45d12bda0d90d636b3a941ed87144df65b4
MuhammadRifkyAkbar <2210631170088@student.unsika.ac.id>
2025-08-17 11:42:59 +0700

    fix(blockchain): update import paths for OpenZeppelin contracts
    - Fix import paths to use ../node_modules/@openzeppelin/contracts
    - Ensure proper contract compilation with correct dependency references
    - Maintain compatibility with local OpenZeppelin installation

--- blockchain/contracts/TrustChainAuthenticityNFT.sol
+++ blockchain/contracts/TrustChainAuthenticityNFT.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "../node_modules/@openzeppelin/contracts/access/AccessControl.sol";
 
 /**
  * @title TrustChainAuthenticityNFT
